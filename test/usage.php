0228aa8b2f7a601a8a9cf0758329762953e3a468
Mirek Ratman <mirek.ratman@modgen.net>
2015-05-25 12:41:49 +0200

    composer autoload update

--- test/usage.php
+++ test/usage.php
@@ -9,8 +9,8 @@
 ERROR_REPORTING(E_ALL);
 require __DIR__ . "/../src/loader.php";
 
-$db = '';
-$key = '';
+$db = 'shopexpo-test';
+$key = 'DyioS5vct4fyqbjjr7Yno8dUFALYjAZe0JP3yR65aCNdtbjk92F9gxU1yDAVR7QS';
 $classApiClient = new \Recommender\Api\Client($db, $key);
 
 $products = array(
